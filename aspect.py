77189af74c2097333a6c6f9a72c05f9ac0c2c72d
Semio Zheng <prairy.long@gmail.com>
2015-02-15 12:43:43 +0800

    add aspect stationery check function

--- aspect.py
+++ aspect.py
@@ -79,3 +79,81 @@ def next_aspect(planet1, planet2, asps=None,
         start = ne
 
     return res.sort_index().head(num)
+
+def previous_aspect(planet1, planet2, asps=None,
+                start=None, num=1, freq='3H', scale=1):
+    """get previous n aspects"""
+
+    if not start:
+        start = datetime.today()
+    elif type(start) is str:
+        start = pd.to_datetime(start)
+
+    if not asps:
+        asps = [0, 60, 90, 120, 180]
+    step = timedelta(days=30)
+    res  = pd.Series([])
+
+    while len(res) < num:
+        pr  = start - step
+        asp = aspect_list2(planet1, planet2, pr, start, asps, freq, scale)
+        if len(asp) >= 1:
+            res = pd.concat([res, asp])
+        start = pr
+
+    return res.sort_index().head(num)
+
+def revert(n):
+    """return the remainder of the given aspect in the 360 cycle.
+       e.g. revert(30) == 330
+    """
+    if n > 0 and n < 360:
+        return 360 - n
+    else :
+        raise ValueError
+
+def next_aspect_states(planet1, planet2, start=None, 
+                        num=1, asps=None, freq='3H', scale=1):
+    """return aspect state changes(station/direction) of given planets 
+       within given time span.
+    """
+    if not start:
+        start = datetime.today()
+    elif type(start) is str:
+        start = pd.to_datetime(start)
+    if not asps:
+        asps = [0, 60, 90, 120, 180]
+
+    res = dict()
+
+    # check current state
+    pre_asps = previous_aspect(planet1, planet2, start=start, asps=asps, freq=freq, 
+                                    scale=scale, num=2)
+    if pre_asps[0] == pre_asps[1]:
+        current_stat = 1 # stationery
+    else:
+        current_stat = 0 # direction
+
+    # now get next states
+    pre_asp = pre_asps[-1]
+    while len(res) < num:
+        next_asps = next_aspect(planet1, planet2, start=start, asps=asps, 
+                                num=1, freq=freq, scale=scale)
+        next_asp = next_asps[-1]
+        if current_stat and next_asp != pre_asp:
+            res[start] = 's -> d'
+            current_stat = 0
+        elif not current_stat and next_asp == pre_asp:
+            res[start] = 'd -> s'
+            current_stat = 1
+
+        start   = next_asps.index[-1]
+        pre_asp = next_asp
+
+    return pd.Series(res)
+
+
+    
+
+
+
---
+++ plot.py
@@ -0,0 +1,8 @@
+
+
+def draw(data, planet1, planet2, start, end, asps, scale):
+  """draw a plot with matplotlib using given data then annotate with the aspect
+     with given planets.
+  """
+
+  pass
