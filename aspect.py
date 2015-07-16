1cd469098bcdd8eb0d012120a270c71c48cf55e6
Semio Zheng <prairy.long@gmail.com>
2015-07-16 15:06:27 +0800

    next aspect state function

--- aspect.py
+++ aspect.py
@@ -1,6 +1,8 @@
+'''aspect related functions'''
 from location import *
 import pandas as pd
 from datetime import datetime, timedelta
+import numpy as np
 
 
 def aspected(x):
@@ -112,9 +114,9 @@ def revert(n):
     else :
         raise ValueError
 
-def next_aspect_states(planet1, planet2, start=None, 
+def next_aspect_states(planet1, planet2, start=None,
                         num=1, asps=None, freq='3H', scale=1):
-    """return aspect state changes(station/direction) of given planets 
+    """return aspect state changes(station/direction) of given planets
        within given time span.
     """
     if not start:
@@ -125,35 +127,24 @@ def next_aspect_states(planet1, planet2, start=None,
         asps = [0, 60, 90, 120, 180]
 
     res = dict()
+    # get last asp
+    pre_asp = previous_aspect(planet1, planet2, start=start, asps=asps, freq=freq,
+                                    scale=scale, num=1)
 
-    # check current state
-    pre_asps = previous_aspect(planet1, planet2, start=start, asps=asps, freq=freq, 
-                                    scale=scale, num=2)
-    if pre_asps[0] == pre_asps[1]:
-        current_stat = 1 # stationery
-    else:
-        current_stat = 0 # direction
-
-    # now get next states
-    pre_asp = pre_asps[-1]
     while len(res) < num:
-        next_asps = next_aspect(planet1, planet2, start=start, asps=asps, 
-                                num=1, freq=freq, scale=scale)
-        next_asp = next_asps[-1]
-        if current_stat and next_asp != pre_asp:
-            res[start] = 's -> d'
-            current_stat = 0
-        elif not current_stat and next_asp == pre_asp:
-            res[start] = 'd -> s'
-            current_stat = 1
+        next_asps = next_aspect(planet1, planet2, start=start, asps=asps,
+                                num=10, freq=freq, scale=scale)
 
-        start   = next_asps.index[-1]
-        pre_asp = next_asp
+        asps_list = pd.concat([pre_asp, next_asps])
+        stay_list = pd.rolling_apply(asps_list, 2, lambda xs: True if xs[0] == xs[1] else np.nan)
+        stay_list = stay_list.dropna()
+        for i in range(len(stay_list)):
+            i0 = asps_list.index.get_loc(stay_list.index[i]) - 1
+            i1 = i0 + 1
+            res[asps_list.index[i0]] = -1
+            res[asps_list.index[i1]] = 1
+
+        pre_asp = asps_list[-1:]
+        start = asps_list.index[-1]
 
     return pd.Series(res)
-
-
-    
-
-
-
